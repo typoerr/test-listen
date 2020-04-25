f9b287f44480fb99e7bf2bbfd8d289652a0fecc5
typoerr <masahirokato.dev@gmail.com>
2020-04-25 16:43:08 +0900

    chore: fix build config

--- rollup.config.js
+++ rollup.config.js
@@ -20,13 +20,6 @@ export default {
       format: 'es',
       sourcemap: true,
     },
-    {
-      dir: 'dist',
-      // name: '',
-      entryFileNames: '[name].umd.js',
-      format: 'umd',
-      sourcemap: true,
-    },
   ],
   context: 'this',
   plugins: [
