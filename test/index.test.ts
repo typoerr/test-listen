f565f7016eb5bfdc6208048db836034b5b6d5204
typoerr <masahirokato.dev@gmail.com>
2020-04-26 10:19:44 +0900

    test: add express test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,7 @@ import test from 'ava'
 import listen from '@/index'
 import http from 'http'
 import fetch from 'node-fetch'
+import express from 'express'
 
 const listener = (req: http.IncomingMessage, res: http.ServerResponse) => {
   res.end(req.url)
@@ -19,5 +20,15 @@ test('listen http server', (t) => {
   const server = http.createServer(listener)
   return listen(server, async function () {
     t.assert(this === server)
+  })
+})
+
+test('express', (t) => {
+  const server = express()
+  server.use(listener)
+  return listen(server, async (url) => {
+    const res = await fetch(url + '/pathname')
+    t.is(res.status, 200)
+    t.is(await res.text(), '/pathname')
   })
 })
